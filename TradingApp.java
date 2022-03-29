5577a1b71ee07eb02efb84abd348806265679370
Jonathan <mangofusion3@gmail.com>
2022-03-29 02:26:00 +0100

    GUI Menu buttons added

--- TradingApp.java
+++ TradingApp.java
@@ -4,38 +4,59 @@ import java.util.Random;
 
 public class TradingApp extends Frame
 {
-    private Label test;
+    private Label balanceText;
+    private Button portfolioButton;
+    private Button depositButton;
+    private Button withdrawButton;
 
     public TradingApp()
     {
         super("Trading Simulator");
         Account account = new Account();
-        account.addMoney(1435.54);
-        Bank b = new Bank();
-        try{b.deposit(300);} catch (SmallDepositException err) {}
-        System.out.println(b.getBalance());
-        TransferRecord tr = new TransferRecord(100);
-        System.out.println(tr.toJSON());
+        account.addMoney(1000);
+        System.out.println("1. Buy Stock\n2. Sell Stock\n3. View Portfolio\n4. Deposit\n5. Withdraw");
 
-        System.out.println("1. Buy Stock\n2. Sell Stock\n3. View Portfolios\n4. Deposit\n5. Withdraw");
         this.setLayout(new FlowLayout());
 
         // Styles
         this.setBackground(new Color(0, 128, 128));
 
-        test = new Label(account.getConvertedBalance(), Label.CENTER);
-        test.setBackground(Color.RED);
-        test.setForeground(Color.WHITE);
-        test.setPreferredSize(new Dimension(250, 100));
-        this.add(test);
+        balanceText = new Label("Balance: " + account.getConvertedBalance(), Label.CENTER);
+        balanceText.setForeground(Color.WHITE);
+        //balanceText.setPreferredSize(new Dimension(250, 100));
+        this.add(balanceText);
 
-        Button test1 = new Button("Update BTC price");
+        portfolioButton = new Button("View Portfolio");
+        portfolioButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                balanceText.setText("Portfolio");
+            }
+        });
+        this.add(portfolioButton);
+
+        depositButton = new Button("Deposit");
+        depositButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                balanceText.setText("Deposit");
+            }
+        });
+        this.add(depositButton);
+
+        withdrawButton = new Button("Withdraw");
+        withdrawButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                balanceText.setText("Withdraw");
+            }
+        });
+        this.add(withdrawButton);
+
+        Button test1 = new Button("See Balance");
         test1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Random rand = new Random();
                 account.addMoney(100000);
                 StockAsset a = new StockAsset("Bitcoin", "BTC", 40000 + rand.nextDouble() * 1000);
-                test.setText(a.toString() + ": " + a.getCurrencyUnitValue());
+                balanceText.setText(a.toString() + ": " + a.getCurrencyUnitValue());
             }
         });
 
